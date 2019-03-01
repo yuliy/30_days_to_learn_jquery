aaa46f4e015ed8c8c04bcb833635c5188e8c1bdf
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-03-01 23:43:48 +0300

    @

---
+++ 3_effects/lesson14/slider.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    var sliderUL =  $('div.slider').children('ul'),
+        imgs = sliderUL.find('img'),
+        imgWidth = imgs.first().width, // 150
+        imgsLength = imgs.length,   // 4
+        current = 1,
+        totalImgsWidth = imgWidth * imgsLength; // 600
+
+    //
+});
+
